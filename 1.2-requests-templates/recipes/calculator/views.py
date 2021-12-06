5366294331fc69bf24a39bec46b5a0754b42a09c
SamayaA <asadullaevasamaya@gmail.com>
2021-12-06 19:34:09 +0300

    Function and render to calculate amount of ingredients

--- 1.2-requests-templates/recipes/calculator/views.py
+++ 1.2-requests-templates/recipes/calculator/views.py
@@ -1,3 +1,4 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
 
 DATA = {
@@ -16,15 +17,14 @@ DATA = {
         'сыр, ломтик': 1,
         'помидор, ломтик': 1,
     },
-    # можете добавить свои рецепты ;)
 }
 
-# Напишите ваш обработчик. Используйте DATA как источник данных
-# Результат - render(request, 'calculator/index.html', context)
-# В качестве контекста должен быть передан словарь с рецептом:
-# context = {
-#   'recipe': {
-#     'ингредиент1': количество1,
-#     'ингредиент2': количество2,
-#   }
-# }
+def get_ingredients(request, dish):
+    dish_ingredients = DATA.get(dish)
+    servings = int(request.GET.get("servings", 1))
+    for i in dish_ingredients:
+        dish_ingredients[i] *= servings
+    context = {
+        'recipe': dish_ingredients,
+    }
+    return render(request,'calculator/index.html', context)

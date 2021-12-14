46d618cb4bd8a552bb9dcb7003007a819f3cc48f
SamayaA <asadullaevasamaya@gmail.com>
2021-12-14 22:17:47 +0300

    Created a relationship (table) Phone

--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -1,6 +1,12 @@
 from django.db import models
+from django.db.models.fields import BooleanField, DateField, SlugField
 
 
 class Phone(models.Model):
-    # TODO: Добавьте требуемые поля
-    pass
+    name = models.CharField(max_length=50)
+    price = models.IntegerField()
+    image = models.CharField(max_length=250)
+    release_date = models.DateField()
+    lte_exists = models.BooleanField()
+    slug = models.SlugField()
+

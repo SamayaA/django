055929f0ad3f9fbd6f463dd92b74850448f4214c
SamayaA <asadullaevasamaya@gmail.com>
2021-12-23 23:10:38 +0300

    Defined functions. Pagination doesn't def

--- 2.1-databases/models_list_displaying/books/views.py
+++ 2.1-databases/models_list_displaying/books/views.py
@@ -1,7 +1,22 @@
+from django.db.models.fields import DateField
 from django.shortcuts import render
+from django.core.paginator import Paginator
+
+from books.models import Book
 
 
 def books_view(request):
     template = 'books/books_list.html'
-    context = {}
+    books = Book.objects.all()
+    context = {
+        "books": books
+    }
+    return render(request, template, context)
+
+def book_view(request, pub_date: DateField):
+    template = 'books/books_list.html'
+    books = [Book.objects.get(pub_date=pub_date)]
+    context = {
+        "books": books,
+    }
     return render(request, template, context)

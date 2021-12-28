926a2bd7828d017baca9d950eb72f358581aa95c
SamayaA <asadullaevasamaya@gmail.com>
2021-12-28 18:04:14 +0300

    Added list comprehension for dates

--- 2.1-databases/models_list_displaying/books/views.py
+++ 2.1-databases/models_list_displaying/books/views.py
@@ -20,8 +20,10 @@ def book_view(request, pub_date: DateField):
     pagination = Paginator(books, 1)
     current_page = books.index(Book.objects.get(pub_date=pub_date))+1
     page = pagination.get_page(current_page)
+    list_dates = [date.pub_date for date in books]
     context = {
         "books": page.object_list,
         "page": page,
+        "dates": list_dates,
     }
     return render(request, template, context)

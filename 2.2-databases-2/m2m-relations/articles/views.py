e19dd16649f349c526aaa90e03334c510ed59b4b
SamayaA <asadullaevasamaya@gmail.com>
2022-02-08 20:12:29 +0300

    Deleted unneccessary import files

--- 2.2-databases-2/m2m-relations/articles/views.py
+++ 2.2-databases-2/m2m-relations/articles/views.py
@@ -1,13 +1,12 @@
 from django.shortcuts import render
 
-from articles.models import Article, Relationship
+from articles.models import Article
 
 
 def articles_list(request):
     template = 'articles/news.html'
     ordering = '-published_at'
     articles = Article.objects.order_by(ordering).all()
-    
     context = {
         'object_list':articles,
     }

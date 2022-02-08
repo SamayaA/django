9282406a05b376d0e994411f3f701dae75ee38c3
SamayaA <asadullaevasamaya@gmail.com>
2022-02-08 20:11:04 +0300

    Changed name of Relationship to Scopes added related_name s

--- 2.2-databases-2/m2m-relations/articles/models.py
+++ 2.2-databases-2/m2m-relations/articles/models.py
@@ -16,7 +16,7 @@ class Article(models.Model):
     text = models.TextField(verbose_name='Текст')
     published_at = models.DateTimeField(verbose_name='Дата публикации')
     image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
-    scopes = models.ManyToManyField(Scope, through="Relationship", verbose_name="Тематика статьи")
+    tags = models.ManyToManyField(Scope, through='Scopes', verbose_name="Тематика статьи", related_name="relation")
 
     class Meta:
         verbose_name = 'Статья'
@@ -24,12 +24,12 @@ class Article(models.Model):
         ordering = ['-published_at']
 
     def __str__(self):
-        return self.title
+        return f"{self.title}"
 
-class Relationship (models.Model):
-    tag = models.ForeignKey(Scope, on_delete= models.CASCADE, verbose_name="Раздел")
-    article = models.ForeignKey(Article, on_delete= models.CASCADE, verbose_name="Статья")
-    is_main = models.BooleanField(verbose_name="Основной тег")
+class Scopes (models.Model):
+    tag = models.ForeignKey(Scope, on_delete= models.CASCADE, verbose_name="Раздел", related_name="scopes")
+    article = models.ForeignKey(Article, on_delete= models.CASCADE, verbose_name="Статья", related_name="scopes")
+    is_main = models.BooleanField(verbose_name="Основной тег",name="is_main")
 
-    def __srt__(self):
+    def __str__(self):
         return '{0}  {1}'.format(self.tag, self.is_main)

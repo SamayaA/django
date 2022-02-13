ddddd58515512b11b14ddc90461617bb9e70268c
SamayaA <asadullaevasamaya@gmail.com>
2022-02-13 23:37:36 +0300

    Registered

--- 3.1-drf-intro/smart_home/measurement/admin.py
+++ 3.1-drf-intro/smart_home/measurement/admin.py
@@ -1,3 +1,13 @@
+from csv import list_dialects
 from django.contrib import admin
 
+from measurement.models import Measurement, Sensor
+
 # Register your models here.
+@admin.register(Sensor)
+class SensorAdmin(admin.ModelAdmin):
+    list_display = ("name", "description")
+
+@admin.register(Measurement)
+class MeasurementAdmin(admin.ModelAdmin):
+    list_display = ("id","temperature", "created_at")

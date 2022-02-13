8bb1f6b5aa4ca51f72ce72045c2e455366bc98f4
SamayaA <asadullaevasamaya@gmail.com>
2022-02-13 23:38:18 +0300

    Defined models

--- 3.1-drf-intro/smart_home/measurement/models.py
+++ 3.1-drf-intro/smart_home/measurement/models.py
@@ -1,3 +1,12 @@
+from pyexpat import model
 from django.db import models
 
 # TODO: опишите модели датчика (Sensor) и измерения (Measurement)
+class Sensor(models.Model):
+    name = models.CharField(max_length=80, default="name")
+    description = models.TextField()
+
+class Measurement(models.Model):
+    temperature = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    sensor = models.ForeignKey(Sensor, related_name="measurements", on_delete=models.CASCADE, default=1)

c3acc7723f68b9d49d3d01606f2db98ccfbc2cf7
SamayaA <asadullaevasamaya@gmail.com>
2022-02-17 17:02:02 +0300

    Defined classes to GET, POST, PATCH methods using generics APIView classes

--- 3.1-drf-intro/smart_home/measurement/views.py
+++ 3.1-drf-intro/smart_home/measurement/views.py
@@ -1,2 +1,24 @@
-# TODO: опишите необходимые обработчики, рекомендуется использовать generics APIView классы:
-# TODO: ListCreateAPIView, RetrieveUpdateAPIView, CreateAPIView
+from django.shortcuts import get_object_or_404
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView, CreateAPIView
+
+from .serializers import SensorDetailSerializer, MeasurementSerializer, SensorSerializer
+from measurement.models import Measurement, Sensor
+
+#GET and POST methods for Sensor model
+class SensorView(ListCreateAPIView):
+    queryset = Sensor.objects.all()
+    serializer_class = SensorSerializer
+
+#GET, POST, PATCH methods for single Sensor model instance
+class SingleSensorView(RetrieveUpdateAPIView):
+    queryset = Sensor.objects.all()
+    serializer_class = SensorDetailSerializer
+
+#POST method for Measurement model
+class MeasurementView(CreateAPIView):
+    queryset = Measurement.objects.all()
+    serializer_class = MeasurementSerializer
+
+    def perform_create(self, serializer):
+        sensor = get_object_or_404(Sensor, id=self.request.data.get('sensor'))
+        return serializer.save(sensor=sensor)

37c010749f63e67794dda1d558fc908e7b0d1585
SamayaA <asadullaevasamaya@gmail.com>
2022-02-26 21:41:25 +0300

    Registered models

--- 3.2-crud/stocks_products/logistic/admin.py
+++ 3.2-crud/stocks_products/logistic/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from logistic.models import Product, StockProduct, Stock
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "description")
+
+@admin.register(Stock)
+class StockAdmin(admin.ModelAdmin):
+    list_display = ("id", "address")
+
+@admin.register(StockProduct)
+class StockProductAdmin(admin.ModelAdmin):
+    list_display = ("id", "stock", "product", "quantity", "price") 

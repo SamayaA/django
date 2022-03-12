ab44e14f532a54d6f8b2e04dd583797813260264
SamayaA <asadullaevasamaya@gmail.com>
2022-03-12 18:47:15 +0300

    Added validation students per course

--- 3.4-django-testing/django_testing/students/serializers.py
+++ 3.4-django-testing/django_testing/students/serializers.py
@@ -1,4 +1,6 @@
+from urllib import request
 from rest_framework import serializers
+from django_testing.settings import MAX_STUDENTS_PER_COURSE
 
 from students.models import Course
 
@@ -8,3 +10,13 @@ class CourseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Course
         fields = ("id", "name", "students")
+
+    def validate(self, data):
+        students = data.get("students")
+        if  type(students)==list and len(students) >= MAX_STUDENTS_PER_COURSE:
+            raise serializers.ValidationError(
+                f"Only {MAX_STUDENTS_PER_COURSE} students allowed in course"
+                )
+        return data
+
+            
